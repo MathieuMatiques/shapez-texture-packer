382e68c1917727b11d7c1a0e96c9485c9ac0eb2e
Mathieu Matiques <mathieummatiques@gmail.com>
2026-01-20 14:30:30 -0800

    Optimize binary size a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,67 @@
+#![feature(iterator_try_collect)]
 use crunch::{Item, Rotation};
 use image::{
     ImageReader, RgbaImage,
     imageops::{self, FilterType},
 };
+use nanoserde::SerJson;
+use neon::{
+    handle::Handle,
+    object::Object,
+    prelude::FunctionContext,
+    result::NeonResult,
+    types::{JsArray, JsNumber, JsObject, JsString, Value},
+};
 use rayon::prelude::*;
-use serde::{Deserialize, Serialize};
+// use orx_parallel::*;
+// use serde::Serialize;
 use std::{collections::BTreeMap, path::Path, time::Instant};
 use walkdir::WalkDir;
 
 #[neon::export]
-fn hello(source: String, dest: String, name: String, config: String) {
-    other(source, dest, name, config).unwrap()
+fn hello(
+    cx: &mut FunctionContext,
+    source: String,
+    dest: String,
+    name: String,
+    config: Handle<JsObject>,
+) -> NeonResult<()> {
+    let config = Config {
+        padding_x: config.prop(cx, "paddingX").get::<f64>()? as u32,
+        padding_y: config.prop(cx, "paddingY").get::<f64>()? as u32,
+        max_width: config.prop(cx, "maxWidth").get::<f64>()? as u32,
+        max_height: config.prop(cx, "maxHeight").get::<f64>()? as u32,
+        scale: config
+            .prop(cx, "scale")
+            .get::<Handle<JsArray>>()?
+            .to_vec(cx)?
+            .iter()
+            .map(|val| {
+                val.downcast::<JsNumber, _>(cx)
+                    .map(|string| string.value(cx))
+            })
+            .try_collect()
+            .unwrap(),
+        scale_suffix: config
+            .prop(cx, "scaleSuffix")
+            .get::<Handle<JsArray>>()?
+            .to_vec(cx)?
+            .iter()
+            .map(|val| {
+                val.downcast::<JsString, _>(cx)
+                    .map(|string| string.value(cx))
+            })
+            .try_collect()
+            .unwrap(),
+    };
+    other(source, dest, name, config).unwrap();
+    Ok(())
 }
 
-pub fn other(source: String, dest: String, name: String, config: String) -> anyhow::Result<()> {
-    let start = Instant::now();
+pub fn other(source: String, dest: String, name: String, config: Config) -> anyhow::Result<()> {
+    // let start = Instant::now();
 
-    let config: Config = serde_json::from_str(&std::fs::read_to_string(&config)?)?;
+    // let config: Config = serde_json::from_str(&std::fs::read_to_string(&config)?)?;
     // dbg!(&config);
     let sources: Vec<_> = WalkDir::new(&source)
         .into_iter()
@@ -71,21 +116,23 @@ pub fn other(source: String, dest: String, name: String, config: String) -> anyh
                 .map(|item| {
                     let (ref img, trimmed, ref trimmed_loc_dims) = loaded_imgs[&item.data];
 
+                    let scaled_trimmed_loc_dims = LocationDimensions {
+                        x: (trimmed_loc_dims.x as f64 * scale).round() as u32,
+                        y: (trimmed_loc_dims.y as f64 * scale).round() as u32,
+                        w: (trimmed_loc_dims.w as f64 * scale).round() as u32,
+                        h: (trimmed_loc_dims.h as f64 * scale).round() as u32,
+                    };
+
                     let sprite_data = SpriteData {
                         frame: LocationDimensions {
                             x: item.rect.x as u32 + config.padding_x / 2,
                             y: item.rect.y as u32 + config.padding_y / 2,
-                            w: (trimmed_loc_dims.w as f64 * scale).round() as u32,
-                            h: (trimmed_loc_dims.h as f64 * scale).round() as u32,
+                            w: scaled_trimmed_loc_dims.w,
+                            h: scaled_trimmed_loc_dims.h,
                         },
                         rotated: false,
                         trimmed,
-                        sprite_source_size: LocationDimensions {
-                            x: (trimmed_loc_dims.x as f64 * scale).round() as u32,
-                            y: (trimmed_loc_dims.y as f64 * scale).round() as u32,
-                            w: (trimmed_loc_dims.w as f64 * scale).round() as u32,
-                            h: (trimmed_loc_dims.h as f64 * scale).round() as u32,
-                        },
+                        sprite_source_size: scaled_trimmed_loc_dims.clone(),
                         source_size: Dimensions {
                             w: (img.width() as f64 * scale).round() as u32,
                             h: (img.height() as f64 * scale).round() as u32,
@@ -102,12 +149,12 @@ pub fn other(source: String, dest: String, name: String, config: String) -> anyh
 
                     let downsized = imageops::resize(
                         &cropped.to_image(),
-                        (trimmed_loc_dims.w as f64 * scale).round() as u32,
-                        (trimmed_loc_dims.h as f64 * scale).round() as u32,
+                        scaled_trimmed_loc_dims.w,
+                        scaled_trimmed_loc_dims.h,
                         FilterType::Triangle,
                     );
 
-                    (item, sprite_data, downsized)
+                    (item.data, sprite_data, downsized)
                 })
                 .collect();
 
@@ -115,14 +162,14 @@ pub fn other(source: String, dest: String, name: String, config: String) -> anyh
 
             let mut frames = BTreeMap::new();
 
-            for (item, sprite_data, downsized) in processed_sprites {
-                frames.insert(item.data, sprite_data);
+            for (key, sprite_data, downsized) in processed_sprites {
                 imageops::replace(
                     &mut output,
                     &downsized,
-                    item.rect.x as i64 + (config.padding_x / 2) as i64,
-                    item.rect.y as i64 + (config.padding_y / 2) as i64,
+                    sprite_data.frame.x as i64,
+                    sprite_data.frame.y as i64,
                 );
+                frames.insert(key, sprite_data);
             }
 
             let meta = MetaData {
@@ -138,13 +185,14 @@ pub fn other(source: String, dest: String, name: String, config: String) -> anyh
             let atlas_data = AtlasData { frames, meta };
             std::fs::write(
                 Path::new(&dest).join(name.clone() + scale_suffix.as_str() + ".json"),
-                serde_json::to_string(&atlas_data)?,
+                // serde_json::to_string(&atlas_data)?,
+                atlas_data.serialize_json(),
             )?;
             output.save(Path::new(&dest).join(name.clone() + scale_suffix.as_str() + ".png"))?;
             Ok(())
         })?;
-    let end = Instant::now();
-    println!("{:?}", end - start);
+    // let end = Instant::now();
+    // println!("{:?}", end - start);
 
     Ok(())
 }
@@ -200,48 +248,50 @@ fn trim(img: &RgbaImage) -> (bool, LocationDimensions) {
     )
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Config {
-    _pot: bool,
-    padding_x: u32,
-    padding_y: u32,
-    _edge_padding: bool,
-    _rotation: bool,
-    max_width: u32,
-    max_height: u32,
-    _use_indexes: bool,
-    _alpha_threshold: u8,
-    _strip_whitespace_x: bool,
-    _strip_whitespace_y: bool,
-    duplicate_padding: bool,
-    _alias: bool,
-    _fast: bool,
-    _limit_memory: bool,
-    _combine_subdirectories: bool,
-    _flatten_paths: bool,
-    _bleed_iterations: u32,
-    scale: Vec<f64>,
-    scale_suffix: Vec<String>,
+#[derive(Debug)]
+// #[serde(rename_all = "camelCase")]
+pub struct Config {
+    // _pot: bool,
+    pub padding_x: u32,
+    pub padding_y: u32,
+    // _edge_padding: bool,
+    // _rotation: bool,
+    pub max_width: u32,
+    pub max_height: u32,
+    // _use_indexes: bool,
+    // _alpha_threshold: u8,
+    // _strip_whitespace_x: bool,
+    // _strip_whitespace_y: bool,
+    // duplicate_padding: bool,
+    // _alias: bool,
+    // _fast: bool,
+    // _limit_memory: bool,
+    // _combine_subdirectories: bool,
+    // _flatten_paths: bool,
+    // _bleed_iterations: u32,
+    pub scale: Vec<f64>,
+    pub scale_suffix: Vec<String>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(SerJson, Debug)]
 struct AtlasData {
     frames: BTreeMap<String, SpriteData>,
     meta: MetaData,
 }
 
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "camelCase")]
+#[derive(SerJson, Debug)]
+// #[serde(rename_all = "camelCase")]
 struct SpriteData {
     frame: LocationDimensions,
     rotated: bool,
     trimmed: bool,
+    #[nserde(rename = "spriteSourceSize")]
     sprite_source_size: LocationDimensions,
+    #[nserde(rename = "sourceSize")]
     source_size: Dimensions,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(SerJson, Debug)]
 struct MetaData {
     image: String,
     format: String,
@@ -249,7 +299,7 @@ struct MetaData {
     scale: String,
 }
 
-#[derive(Serialize, Debug, Clone)]
+#[derive(SerJson, Debug, Clone)]
 struct LocationDimensions {
     x: u32,
     y: u32,
@@ -257,7 +307,7 @@ struct LocationDimensions {
     h: u32,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(SerJson, Debug)]
 struct Dimensions {
     w: u32,
     h: u32,
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+#![feature(iterator_try_collect)]
 mod lib;
-use lib::other;
+use lib::{Config, other};
 
 fn main() {
     other(
         "../res_raw".to_owned(),
         "/tmp/atlas".to_owned(),
         "atlas0".to_owned(),
-        "../res_raw/atlas.json".to_owned(),
+        Config {
+            padding_x: 2,
+            padding_y: 2,
+            max_width: 2048,
+            max_height: 2048,
+            scale: vec![0.25, 0.5, 0.75],
+            scale_suffix: vec!["_lq".to_owned(), "_mq".to_owned(), "_hq".to_owned()],
+        },
     )
     .unwrap();
 }
